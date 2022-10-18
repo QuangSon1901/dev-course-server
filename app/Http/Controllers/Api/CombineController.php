7d47b70c33e5b80a10a8f355bcb992a32334981c
QuangSon1901 <quangsonbmt760@gmail.com>
2022-10-18 14:03:25 +0700

    Sdasd

--- app/Http/Controllers/Api/CombineController.php
+++ app/Http/Controllers/Api/CombineController.php
@@ -3,21 +3,26 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Course;
 use App\Models\Program;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class CombineController extends Controller
 {
     public function show() {
-        $programs = Program::all();
+        $programs = Program::select('id', 'name', 'slug')->get();
         foreach ($programs as $item) {
             $item['category_courses'] = $item->category_courses;
         }
 
+        $new_courses = Course::select('id', 'name', 'image', 'price', 'slug')->where('created_at', '>=', Carbon::now()->subMonth(1))->take(10)->get();
+        
+
         $response = [
             'status' => 201,
             'success' => 'success',
-            'menu_programs' => $programs
+            'encrypt' => encrypt(['programs' => $programs, 'new_courses' => $new_courses])
         ];
 
         return response($response, 201);
